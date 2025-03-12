21d15d2d8e32d051a4f850917c4309451b6494df
zuhdil <zuhdil@gmail.com>
2025-03-12 12:15:26 +0700

    Add mailpit service

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,1 +1,49 @@
-module.exports = () => ({});
+module.exports = ({ env }) => {
+  // `PLUGIN_PROVIDERS` allows the use of local providers in production build containers for local testing.
+  const profile = env.oneOf(
+    'PLUGIN_PROVIDERS',
+    ['local', 'external'],
+    'external'
+  );
+
+  const emails = {
+    local: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: 'mailpit',
+        port: 1025,
+        ignoreTLS: true,
+        auth: false,
+      },
+    },
+    external: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env('SMTP_PORT'),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        secure: true, // Changed to false for port 587
+        requireTLS: true, // Added this
+        tls: {
+          ciphers: 'SSLv3', // Added this
+          rejectUnauthorized: false, // Added this
+        },
+      },
+    },
+  };
+
+  return {
+    email: {
+      config: {
+        ...emails[profile],
+        settings: {
+          defaultFrom: env('SMTP_FROM'),
+          defaultReplyTo: env('SMTP_FROM'),
+        },
+      },
+    },
+  };
+};
