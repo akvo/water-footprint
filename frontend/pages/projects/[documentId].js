88ffdbac2f3c3666279ef61e309cd60d7938c844
navins94 <nikshingote@gmail.com>
2025-04-03 14:34:00 +0530

    fix api call

--- frontend/pages/projects/[documentId].js
+++ frontend/pages/projects/[documentId].js
@@ -65,7 +65,6 @@ export default function ProjectPage() {
         'populate[7]': 'monitoringReports.file',
         'populate[8]': 'updates',
         'populate[9]': 'updates.image',
-        'filters[updates][publishedAt][$notNull]': true,
       });
 
       if (response?.data && response.data.length > 0) {
