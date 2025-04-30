b25fa3a6be496b26553698923216f5a8cbb8717b
zuhdil <zuhdil@gmail.com>
2025-04-30 16:00:54 +0700

    Fix admin reset password email link

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: '/cms',
+  url: env('BACKEND_URL', '/cms'),
   app: {
     keys: env.array('APP_KEYS'),
   },
